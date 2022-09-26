fd63c75d9a2e3beac4d8e8dffcd9aa3e58415399
Danielle <danielle@Danielles-MacBook-Air.local>
2022-09-25 20:26:39 -0400

    editing to show password

--- README.md
+++ README.md
@@ -18,5 +18,7 @@ Javascript is used to generate and confirm the window prompts. <br>
 
 ## Screenshot
 
+<img width="1440" alt="Screen Shot 2022-09-25 at 8 24 08 PM" src="https://user-images.githubusercontent.com/106556894/192172730-5119cb48-aee3-4634-b026-ef8708343d68.png">
+
 ## Contribution
 Made with ❤️ by Danielle Locklear
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,62 +1,74 @@
 // DOM elements
 var generatePassword = function () {
-    var passlength = 0;
-    var charSet = "";
-    var lowercase = "abcdefghijklmnopqrstuvwxyz";
-    var uppercase = "ABCDEFGHIJKLMNOPRSTUVWXYZ";
+    var passLength = 0;
+    var fullPassword = "";
+    var lowercase = "abcdefghijklmnnopqrstuvwxyz";
+    var uppercase = "ABCDEFGHIJKLMNNOPQRSTUVWXYZ";
     var numeric = "123456789";
-    var special = "!@#$%^&*()-_=+{}[]|,.`~";
-
+    var special = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+  
+    // prompt for password length/check for length
     do {
-        passLength = window.prompt("How many characters would you like? Must have minimum of 8 and Maximum of 128?");
-        if (passLength < 8 || passlength > 128) {
-            alert("You must enter in a number between 8 and 128!");
-        }
-    } while (passlength < 8 || passlength > 128);
-
-do {
-    var lowercaseEl = window.confirm("Would you like to include lowercase letters?")
-    var uppercaseEl = window.confirm("Would you like to include uppercase letters?")
-    var numbersEl = window.confirm("Would you like to include numbers?")
-    var symbolsEl = window.confirm("Would you like to include special characters?")
-
-    if (lowercaseEl) {
-        charSet += lowercase
-    }
-
-    if (uppercaseEl) {
-        charSet += uppercase
+      passLength = window.prompt("How many characters would you like? Minimum of 8 and Maximum of 128");
+      // creating expression to see if passLength is not a number
+      if (passLength < 8 || passLength > 128) {
+        // alerts uers if entry is not between 8 and 128
+        alert("You must enter in a number between 8 and 128!");
       }
+    } while (passLength < 8 || passLength > 128);
   
-    if (numbersEl) {
-        charSet += numeric
-      }
-  
-    if (symbolsEl) {
-        charSet += special
-      }
+    do {
+      // prompt for password criteria
+      var lowercaseEl = window.confirm("Would you like to include lowercase letters?")
+      var uppercaseEl = window.confirm("Would you like to include uppercase letters?")
+      var numberEl = window.confirm("Would you like to include numbers?")
+      var symbolsEl = window.confirm("Would you like to include special characters?")
     
-    if (!uppercaseEl && !lowercaseEl && !numbersEl && !symbolsEl) {
-        window.alert ("You must select at least one criteria")
-    }
-    } while (!uppercaseEl && !lowercaseEl && !numbersEl && !symbolsEl)
-    console.log(charSet)
-
+  
+      // add selected criteria to password pool 
+      if (lowercaseEl) {
+        fullPassword += lowercase
+      }
+  
+      if (uppercaseEl) {
+        fullPassword += uppercase
+      }
+  
+      if (numberEl) {
+        fullPassword += numeric
+      }
+  
+      if (symbolsEl) {
+        fullPassword += special
+      }
+      if (!lowercaseEl && !uppercaseEl && !numberEl && !symbolsEl) {
+        window.alert("You must select at least one criteria.")
+      }
+    } while (!lowercaseEl && !uppercaseEl && !numberEl && !symbolsEl)
+    console.log(fullPassword)
+  
+  
+    // generate password from selected criteria
     var newPass = "";
-    for (var i = 0; i < passlength; i++) {
-        newPass += charSet[Math.floor(Math.random() * charSet.length)]
+    for (var i = 0; i < passLength; i++) {
+      newPass += fullPassword[Math.floor(Math.random() * fullPassword.length)]
     }
     console.log(newPass)
     return newPass;
-};
-
-    var generateBtn = document.querySelector("#generate");
-
-    function writePassword() {
-        var password = generatePassword();
-        var passwordText = document.querySelector("#password");
-
-        passwordText.value = password;
-    }
-
-    generateBtn.addEventListener("click", writePassword);
+  };
+  
+  
+  // Get references to the #generate element
+  var generateBtn = document.querySelector("#generate");
+  
+  // Write password to the #password input
+  function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+  
+    passwordText.value = password;
+  
+  }
+  
+  // Add event listener to generate button
+  generateBtn.addEventListener("click", writePassword);
